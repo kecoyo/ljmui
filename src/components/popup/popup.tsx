16cda1f1d87039c42162b08bcdb217ea07dda20e
杨可可 <kecoyo@163.com>
2022-10-27 19:40:38 +0800

    popup

---
+++ src/components/popup/popup.tsx
@@ -0,0 +1,35 @@
+import React, { FC } from 'react';
+import { mergeProps } from 'antd-mobile/es/utils/with-default-props';
+import AdmPopup, { PopupProps } from 'antd-mobile/es/components/popup';
+import { defaultPopupBaseProps } from 'antd-mobile/es/components/popup/popup-base-props';
+import { useUpdateEffect } from 'ahooks';
+import Event from '../event';
+
+export const EVENT_POPUP_CLOSE = 'event-popup-close';
+
+const defaultProps = {
+  ...defaultPopupBaseProps,
+  position: 'bottom',
+};
+
+export const Popup: FC<PopupProps> = p => {
+  const props = mergeProps(defaultProps, p);
+
+  useUpdateEffect(() => {
+    if (props.onClose) {
+      if (props.visible) {
+        Event.addListener(EVENT_POPUP_CLOSE, props.onClose);
+      } else {
+        Event.removeListener(EVENT_POPUP_CLOSE, props.onClose);
+      }
+    } else if (props.onMaskClick) {
+      if (props.visible) {
+        Event.addListener(EVENT_POPUP_CLOSE, props.onMaskClick);
+      } else {
+        Event.removeListener(EVENT_POPUP_CLOSE, props.onMaskClick);
+      }
+    }
+  }, [props.visible]);
+
+  return <AdmPopup {...props} />;
+};
